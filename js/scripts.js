465fd734d746d3f44a5b2aeca02be7ce64fff35c
Jiwon Han <jiwon1.han@gmail.com>
2020-01-15 12:22:48 -0800

    edit few things regarding jquery and css

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,27 @@ $(document).ready(function() {
     $("ul#Catspeak").append("<li>What does the dog say</li>");
     $("ul#Dogspeak").append("<li>Woof</li>");
     $('li').css('background-color', 'green');
-   
+
     });
- 
+
 
 
   $("button#Cat").click(function() {
     $("ul#Catspeak").append("<li>What does the cat say</li>");
     $("ul#Dogspeak").append("<li>Meow</li>");
-    $("ul#Dogspeak").append('<img src="img/cat.jpg" alt="cat">');
-    
+    $('<img src="img/cat.jpg" alt="cat">').after("ul#Dogspeak");
+
   });
 
  $("button#Pic").click(function(){
-   $("ul#Catspeak").append('<img src="img/cat.jpg" alt="cat">');
-   $( "ul#Catspeak").after("Isn't this cat cute")
+   $("ul#pics").append("<li>Let's add a cat's picure</li>");
+   $("ul#Catspeak").after('<img src="img/cat.jpg" alt="cat">')
+   $("ul#Dogspeak").after('<img srcp"img/dog.jpg alt="dog">')
+ });
+
+ $("button#remove").click(function(){
+   $("img").remove();
+   $("li").remove();
  });
 
 
